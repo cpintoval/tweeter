ae62cf9ea5f1f42777d342243b8044f4318a1d96
Carlos Pinto <cpintovalderrama@gmail.com>
2021-02-23 02:15:12 +0000

    Adding fixed nav, tweet composer, tweets, and styling

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+  $(".form__textarea").on('keyup', function(evt) {
+    let $input   = $(this);
+    let $form    = $input.closest('form');
+    let $counter = $form.find('.form__counter')
+
+    let len = $input.val().length;
+
+    let charsLeft = 140 - len;
+    $counter.html(charsLeft);
+
+    if (charsLeft < 0) {
+      $counter.addClass('form--red');
+    } else {
+      $counter.removeClass('form--red');
+    }
+  });
+});
